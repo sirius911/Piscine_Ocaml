a7beb4eeb15d91ddf749254a75699a852bac1400
clorin <clorin@student.42.fr>
2023-08-31 19:34:03 +0200

    ex03 ...in progress

--- day03/ex03/btree.ml
+++ day03/ex03/btree.ml
@@ -9,37 +9,61 @@
 (*   Updated: 2023/08/30 22:50:21 by clorin           ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
-
+(*14,53*)
 type 'a tree = Nil | Node of 'a * 'a tree * 'a tree
 
-let is_bst (t : 'a tree) : bool = 
-  let rec loop (t : 'a tree) (greater_value : 'a option) (smaller_value : 'a option) =
-    match t with
-    | Node(value,left,right) ->
-      if greater_value != None && Some value > greater_value then false
-      else if smaller_value != None && Some value < smaller_value then false
-      else loop left (Some value) smaller_value && loop right greater_value (Some value) 
-    | Nil -> true
-  in 
-  loop t None None
+let rec height (t: 'a tree) : int = match t with
+  | Nil -> 0
+  | Node (_, Nil, Nil) -> 0
+  | Node (_, left, right) -> 1 + max (height left) (height right)
 
-let is_perfect (t : 'a tree) : bool =
-  let rec loop t d lvl =
-    match t with
-    | Node (a,left,right) -> if left = Nil && right = Nil then (d = (lvl + 1))
-                      else if left = Nil || right = Nil then false
-                      else (loop left d (lvl + 1) && loop right d (lvl + 1))
-    | Nil -> true
+let is_bst (t : 'a tree) : bool = 
+   (*The trick I use is to transform the tree into a list through an infix path, and then check if the list is sorted.*)
+  let rec is_sorted (l:'a list) : bool = 
+    match l with
+    | [] | [_] -> true
+    | x::y::rest -> if x > y then false else is_sorted (y::rest)
   in
-  let rec getdepth t d =
+  let rec get_list_of_tree (t : ('a tree)) : 'a list = 
     match t with
-      Node (a,b,_) -> getdepth b (d + 1)
-    | Nil -> d
+    | Nil -> []
+    | Node(a, left, Nil) -> get_list_of_tree left @ [a]
+    | Node(a, Nil, right) -> [a] @ get_list_of_tree right
+    | Node(a, left, right) ->
+        (get_list_of_tree (left)) @ [a] @ (get_list_of_tree (right))
   in
-  loop t (getdepth t 0) 0
+  is_sorted (get_list_of_tree t)
+
+(* let rec is_perfect (t : 'a tree) : bool =
+  match t with
+  | Nil -> true
+  | Node(a, Nil, Nil) -> true
+  | Node(a, Nil, b) | Node(a, b, Nil) -> false
+  | Node(a, left, right) -> is_perfect left && is_perfect right *)
+   
+
+let is_perfect (tree : 'a tree) : bool =
+  let h = height tree in
+  let rec loop (t: 'a tree) (depth : int) (current_depth : int) : bool =
+    match tree with
+    | Nil -> current_depth = depth
+    | Node (_, left, right) ->
+      loop left depth (current_depth + 1) &&
+      loop right depth (current_depth + 1)
+  in
+  loop tree h 0
+
+let rec is_balanced (t : 'a tree) : bool = match t with
+  | Nil -> true
+  | Node(a, Nil, Nil) -> true
+  | Node(a, Nil, b) | Node(a, b, Nil) -> 
+      if ((height b) > 1) then false else true
+  | Node(a, left, right) -> 
+      if ((abs ((height left) - (height right))) > 1) then false
+      else is_balanced left && is_balanced right
 
 let main () = 
-  let t = Node (10, Node (7, Nil, Nil), Node (11, Nil, Node (12, Node (50, Nil, Nil), Nil))) in (* false *)
+  let t = Node (10, Node (7, Nil, Node(13, Nil, Nil)), Node (11, Nil, Node (12, Node (50, Nil, Nil), Nil))) in (* false *)
   
   let rec print_bst t =
     match t with
@@ -48,16 +72,38 @@ let main () =
   in
 
   print_bst t;
-  Printf.printf "\n%b\n" (is_bst t);
+  Printf.printf " -> %b => " (is_bst t);
+  Printf.printf "%b **> " (is_perfect t);
+  Printf.printf "%b\n" (is_balanced t);
 
   let t = Node (10, Node (7, Nil, Node (9, Nil, Nil)), Node (11, Nil, Node (12, Nil, Nil))) in (* true *)
   print_bst t;
-  Printf.printf "\n%b\n" (is_bst t);
-  Printf.printf "%b\n" (is_perfect t);
+  Printf.printf " -> %b => " (is_bst t);
+  Printf.printf "%b **> " (is_perfect t);
+  Printf.printf "%b\n" (is_balanced t);
 
   let t = Node (10, Node (1, Nil, Nil), Node (20, Nil, Nil)) in (* perfect *)
   print_bst t;
-  Printf.printf "\n%b\n" (is_bst t);
-  Printf.printf "%b\n" (is_perfect t)
+  Printf.printf " -> %b => " (is_bst t);
+  Printf.printf "%b **> " (is_perfect t);
+  Printf.printf "%b\n" (is_balanced t);
+
+  let t = Node (10, Node (5, Node(1, Node(0, Nil, Nil), Node(3, Nil, Nil)), Node(7, Nil, Nil)), Node (20, Node(15, Nil, Nil), Node(25, Nil, Nil))) in (* perfect *)
+  print_bst t;
+  Printf.printf " -> %b => " (is_bst t);
+  Printf.printf "%b **> " (is_perfect t);
+  Printf.printf "%b\n" (is_balanced t);
+
+  let t = Node (10, Node (5, Node(1, Node(0, Nil, Nil), Node(3, Nil, Nil)), Nil), Node (20, Node(15, Nil, Nil), Node(25, Nil, Nil))) in (* perfect *)
+  print_bst t;
+  Printf.printf " -> %b => " (is_bst t);
+  Printf.printf "%b **> " (is_perfect t);
+  Printf.printf "%b\n" (is_balanced t);
+
+  let t = Node (4,Node(3, Node(2, Node(1, Nil, Nil), Nil), Nil), Nil) in
+  print_bst t;
+  Printf.printf " -> %b => " (is_bst t);
+  Printf.printf "%b **> " (is_perfect t);
+  Printf.printf "%b\n" (is_balanced t)
 
 let () = main ()
