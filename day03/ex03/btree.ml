d3da8c003c6950e2143920533b42eabe34135e27
clorin <clorin@student.42.fr>
2023-09-02 11:47:27 +0200

    ex03 ...in progress

--- day03/ex03/btree.ml
+++ day03/ex03/btree.ml
@@ -6,7 +6,7 @@
 (*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2023/08/30 21:48:41 by clorin            #+#    #+#             *)
-(*   Updated: 2023/09/01 19:43:11 by clorin           ###   ########.fr       *)
+(*   Updated: 2023/09/02 11:20:51 by clorin           ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -54,6 +54,47 @@ let rec is_balanced (t : 'a tree) : bool = match t with
       if ((abs ((height left) - (height right))) > 1) then false
       else is_balanced left && is_balanced right
 
+let search_bst (t : 'a tree) (value : 'a) : bool =
+  let rec search (t : 'a tree) (value : 'a) : bool = 
+    match t with
+    | Node(a, left, right) -> if a = value then true
+      else search left value || search right value
+    | Nil -> false
+    in
+  if is_bst t then
+    search t value
+  else
+    begin
+      print_endline("the tree must be a BST !");
+      false
+    end
+
+let add_bst (t : 'a tree) (value : 'a) : ('a tree) =
+  let rec add (t : 'a tree) (value : 'a) : ('a tree) = 
+    match t with
+    | Nil -> Node(value, Nil, Nil)
+    | Node(a, left, right) ->
+      if value < a then Node(a, add left value, right)
+      else if value > a then Node(a, left, add right value)
+      else t
+  in
+  if is_bst t then
+    add t value
+  else  
+    begin
+      print_endline("the tree must be a BST !");
+      t
+    end
+
+let delete_bst (t : 'a tree) (value : 'a) : ('a tree) = 
+  if is_bst t then
+    t
+  else  
+    begin
+      print_endline("the tree must be a BST !");
+      t
+    end
+
 let main () = 
   let t = Node (10, Node (7, Nil, Node(13, Nil, Nil)), Node (11, Nil, Node (12, Node (50, Nil, Nil), Nil))) in (* false *)
   
@@ -67,26 +108,30 @@ let main () =
   Printf.printf " -> %b => " (is_bst t);
   Printf.printf "%b **> " (is_perfect t);
   Printf.printf "%b\n" (is_balanced t);
+  let value_search = 2 in 
+  Printf.printf "%d in tree = %b\n" value_search (search_bst t value_search);
+  let value_search = 50 in 
+  Printf.printf "%d in tree = %b\n" value_search (search_bst t value_search);
 
-  let t = Node (10, Node (7, Nil, Node (9, Nil, Nil)), Node (11, Nil, Node (12, Nil, Nil))) in (* true *)
+  let t = Node (10, Node (7, Nil, Node (9, Nil, Nil)), Node (11, Nil, Node (12, Nil, Nil))) in 
   print_bst t;
   Printf.printf " -> %b => " (is_bst t);
   Printf.printf "%b **> " (is_perfect t);
   Printf.printf "%b\n" (is_balanced t);
 
-  let t = Node (10, Node (1, Nil, Nil), Node (20, Nil, Nil)) in (* perfect *)
+  let t = Node (10, Node (1, Nil, Nil), Node (20, Nil, Nil)) in 
   print_bst t;
   Printf.printf " -> %b => " (is_bst t);
   Printf.printf "%b **> " (is_perfect t);
   Printf.printf "%b\n" (is_balanced t);
 
-  let t = Node (10, Node (5, Node(1, Node(0, Nil, Nil), Node(3, Nil, Nil)), Node(7, Nil, Nil)), Node (20, Node(15, Nil, Nil), Node(25, Nil, Nil))) in (* perfect *)
+  let t = Node (10, Node (5, Node(1, Node(0, Nil, Nil), Node(3, Nil, Nil)), Node(7, Nil, Nil)), Node (20, Node(15, Nil, Nil), Node(25, Nil, Nil))) in
   print_bst t;
   Printf.printf " -> %b => " (is_bst t);
   Printf.printf "%b **> " (is_perfect t);
   Printf.printf "%b\n" (is_balanced t);
 
-  let t = Node (10, Node (5, Node(1, Node(0, Nil, Nil), Node(3, Nil, Nil)), Nil), Node (20, Node(15, Nil, Nil), Node(25, Nil, Nil))) in (* perfect *)
+  let t = Node (10, Node (5, Node(1, Node(0, Nil, Nil), Node(3, Nil, Nil)), Nil), Node (20, Node(15, Nil, Nil), Node(25, Nil, Nil))) in 
   print_bst t;
   Printf.printf " -> %b => " (is_bst t);
   Printf.printf "%b **> " (is_perfect t);
@@ -97,7 +142,15 @@ let main () =
   Printf.printf " -> %b => " (is_bst t);
   Printf.printf "%b **> " (is_perfect t);
   Printf.printf "%b\n" (is_balanced t);
-
+  let value_search = 2 in 
+  Printf.printf "%d in tree = %b\n" value_search (search_bst t value_search);
+  let value_search = 42 in 
+  Printf.printf "%d in tree = %b\n" value_search (search_bst t value_search);
+  let t = add_bst t 5 in
+  print_endline "-------";
+  print_bst t;
+  print_endline "-------";
+  
   let balanced_tree =
     Node(4,
       Node(2,
@@ -109,9 +162,15 @@ let main () =
         Node(7, Nil, Nil)
       )
     ) in
-  print_bst t;
+  print_bst balanced_tree;
   Printf.printf " -> %b => " (is_bst balanced_tree);
   Printf.printf "%b **> " (is_perfect balanced_tree);
-  Printf.printf "%b\n" (is_balanced balanced_tree)
-
+  Printf.printf "%b\n" (is_balanced balanced_tree);
+  let value_search = 4 in 
+  Printf.printf "%d in tree = %b\n" value_search (search_bst balanced_tree value_search);
+  let value_search = 42 in 
+  Printf.printf "%d in tree = %b\n" value_search (search_bst balanced_tree value_search);
+  let t = add_bst balanced_tree 0 in
+  print_bst t
+  
 let () = main ()
